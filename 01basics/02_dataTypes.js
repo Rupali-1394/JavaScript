69a21d28f4da9be058ea2957836aa292b963603a
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-11-03 09:35:49 +0000

    dataTypes in js

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treatall JS code as newer version
+
+//alert(3+3)we are using node js not brower
+
+//console.log(3 + 3); console.log("Rupali") // avoid this this is not a good readable code write code with proper indentation
+
+//data Types
+let name ="Rupali"; // string
+let age =20 ; // int -->range =>2 to power 53
+let islogged = true; /// bool
+
+
+// number ->int
+// bigInt ->for big number
+// string =>""
+// boolean =>true/false
+//null =>stand  alone value
+//undefined => value is not defined yet
+// symbol =>unique
+
+
+//object
+
+console.log(typeof "Rupali");
+console.log(typeof age);
+console.log(typeof null);// this is an interview ques as well
+console.log(typeof undefined);
+

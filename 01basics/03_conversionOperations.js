1dd764234babb00328965ac22f2799c44e4710e8
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-11-04 14:18:26 +0000

    Conversions Operations in js

---
+++ 01basics/03_conversionOperations.js
@@ -0,0 +1,59 @@
+/*
+let score =" 44bjkj ";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);// conversion of string into number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);// NaN means not a number as 44bjkj is not a pure number
+
+// what if null
+let val =null;
+let valConvert = Number(val)
+console.log(typeof valConvert);
+
+// undefined
+let abc = undefined;
+let valInNumber =   (abc);
+console.log(typeof(valInNumber));
+console.log(valInNumber);
+
+*/
+// jo convert nhui ho sakta h usme ourput NaN aa jata h
+
+/*
+Conversion
+"44"=> 44
+"34sf"=>NaN
+true =>1; false => 0;
+
+*/
+// likewise we did in conversion to Number we can do the same for String Bool etc as well 
+// let's do it
+
+
+/*
+let isLoggedIn=7765 ;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+conversion to boolean
+1=true; 0 =>false;
+"" => false; "rup" =>true;
+
+let someNum = 35;
+let stringNum = String(someNum);
+console.log(stringNum);
+console.log(typeof stringNum);
+
+
+*/
+
+// ***************** Operations ******************* \\
+let value = 34;
+let negValue = -value;
+console.log(negValue);

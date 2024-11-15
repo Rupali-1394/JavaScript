aab6cfa959d1aa453be86455a45a8d699d2a9822
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-11-15 14:11:20 +0000

    Data Type Summary

--- 01basics/05_dataTypes.js
+++ 01basics/05_dataTypes.js
@@ -9,3 +9,38 @@ Arrays, Objects, Functions
 
 
 */
+
+const score=100; // dataTypes initially was undefined
+let userEmail; // variable
+
+// defineing a symbol
+const id =Symbol('@#');
+const id2 = Symbol('@#');
+// both are different 
+// lets check if same?
+console.log(id == id2);
+
+// bigInt just add n at last with the number
+const bigNumber = 46546564584765245563464n;
+console.log(typeof(bigNumber));
+
+// array
+const heros = ["Ironman" , "SpiderMan", "loki", "thor"];
+// string
+let Omymy={
+    name: "Rupali",
+    age : 20,
+}
+
+//functions
+const myfunc = function(){
+    console.log( "Hello World");
+}
+
+//let's know there datatypes using typeof
+console.log(typeof(null));
+console.log(typeof(Omymy));
+console.log(typeof(heros));
+console.log(typeof(array));// undefined
+
+console.log(typeof(myfunc));

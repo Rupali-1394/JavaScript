339c9cb242482b166fd24f69b6a5348f6df3adfb
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-11-15 13:44:55 +0000

    Data Types

---
+++ 01basics/05_dataTypes.js
@@ -0,0 +1,11 @@
+/*// Primitive-all primitive are passed by value
+7 types : String, Number , Boolean, null, Symbol, BigInt
+
+
+//Reference type (Non-Primitive)
+Arrays, Objects, Functions 
+
+// here in js there is no int nd float everything is a number
+
+
+*/

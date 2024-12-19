6b08cc20adc877de87ae0f282ecccde30e6e88b8
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-12-19 03:14:26 +0000

    Number and Mathematics

--- 01basics/08_numberMaths.js
+++ 01basics/08_numberMaths.js
@@ -12,7 +12,7 @@ const num2 = new Number(123);
 //console.log(num1.toFixed(2));// used for putting decimal exact to n decimal place
 
 const num3 = 1234.89342;
-//console.log(num3.toPrecision(5));// like estimation nearest to for decimal vslues if applicable
+//console.log(num3.toPrecision(2));// like estimation nearest to for decimal vslues if applicable
 
 //console.log(num3.toExponential(2));
 
@@ -36,12 +36,17 @@ const balance = 100000000;
 // console.log(Math.min(4,8,77));
 
 
-console.log(Math.random());// always gives value between 0 and 1
-console.log((Math.random()*10) +1);// we use this in dice game to get no after rolling the dice
-console.log(Math.floor(Math.random()*10) +1);// jus to keep the lower number
+// console.log(Math.random());// always gives value between 0 and 1
+// console.log((Math.random()*10) +1);// we use this in dice game to get no after rolling the dice
+// console.log(Math.floor(Math.random()*10) +1);// jus to keep the lower number
 
-// now more prominent way to use it that our value should lie between min and max
-const min = 10;
-const max = 20;
+// // now more prominent way to use it that our value should lie between min and max
+// const min = 10;
+// const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min +1) ) + min )
+//console.log(Math.floor(Math.random() * (max - min +1) ) + min )
+
+
+// for ludo game
+const min = 1 ,max =6
+console.log(Math.floor(Math.random()*(max-min+1))+min);

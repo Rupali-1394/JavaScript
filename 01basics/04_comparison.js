79163d6db43c33befbe15a797a22c7546935956f
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-12-19 03:13:03 +0000

    comparisons

--- 01basics/04_comparison.js
+++ 01basics/04_comparison.js
@@ -5,24 +5,24 @@
 // console.log( 2 != 1);
 
 // // above comparison is easy 
-// // it's get difficult when we compare differnt dataTypes
+// // // it's get difficult when we compare differnt dataTypes
 // console.log("2" > 1);
 // console.log("2" >= 1);
 // console.log("2" == 1);
 // console.log("2" != 1);
 //  js has changed their data type to int
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);//  null got converted to 0
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//  null got converted to 0
 // /*in js comparison and equality operators work differntly
 // >= , <= and
 // == , === have  different syntax
 // */
 
-console.log(undefined == 0);
-console.log(undefined <= 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+// console.log(undefined < 0);
 // undefined gives false in all cases
 
 // whenever we check == this do conversion of data types

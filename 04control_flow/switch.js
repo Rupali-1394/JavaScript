f86d643c87bc3970ea45326e38d292a56f897e72
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-12-24 10:44:43 +0000

    Control flow

---
+++ 04control_flow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// same syntax as in c++
+const month = 3
+switch (month){
+    case 1 :
+        console.log("jan");
+        break;
+    case 2 :
+        console.log("feb");
+        break;
+    case 3 :
+        console.log("march");
+        break;
+    case 4 :
+        console.log("april");
+        break;
+    case 5 :
+        console.log("may");
+        break;
+    case 6 :
+        console.log("june");
+        break;
+    default :
+        console.log("Wrong month entered");
+        break;
+}

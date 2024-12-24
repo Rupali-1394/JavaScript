c2c0e5476b711b8389c68750a4375aa1a370b2b6
Rupali Kumari <159249914+Rupali-1394@users.noreply.github.com>
2024-12-24 12:03:14 +0000

    newOperations

---
+++ 05iterations/six.js
@@ -0,0 +1,14 @@
+
+//const coding = ['cpp','js','python', 'ruby','java']
+
+// suppose we want to store the value of we want to store the values can we do it?
+
+// const value = coding.forEach((item) => {
+//     return item;
+// });
+
+// console.log(value);// undefined
+
+const myNums =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+myNums.filter((num) => num >4)
